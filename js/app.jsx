90448e6dd3211c824a2f5ab0ab3e6f8cee560c89
Gjum <code.gjum@gmail.com>
2016-09-23 19:46:09 +0200

    launch

---
+++ js/app.jsx
@@ -0,0 +1,109 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var L = require('leaflet');
+var RL = require('react-leaflet');
+
+var Util = require('./util.js');
+
+const attribution = '<a href="https://dev3map.github.io">dev3map.github.io</a>';
+const tilesUrl = 'https://raw.githubusercontent.com/dev3map/tiles/master/world/';
+
+var mcCRS = L.extend({}, L.CRS.Simple, {
+  transformation: new L.Transformation(1, 0, 1, 0)
+});
+
+class Centered extends React.Component {
+  render() {
+    return (
+      <div className='center-outer full'>
+      <div className='center-middle'>
+      <div className='center-inner'>
+        {this.props.children}
+      </div></div></div>);
+  }
+}
+
+class CoordsDisplay extends React.Component {
+  render() {
+    const [z, x] = Util.intCoords(this.props.cursor);
+    return <div className='coords-display control-box leaflet-control leaflet-control-layers'>
+      {'X ' + x + ' ' + z + ' Z'}</div>;
+  }
+}
+
+class McMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: Util.hashToView(location.hash),
+      cursorPos: L.latLng(0,0),
+    };
+  }
+
+  componentWillMount() {
+    // TODO load json data into state here
+  }
+
+  onmoveend(o) {
+    const stateUrl = '#' + Util.viewToHash(o.target);
+    history.replaceState({}, document.title, stateUrl);
+  }
+
+  onmousemove(o) {
+    this.setState({cursorPos: o.latlng});
+  }
+
+  render() {
+    var tileBounds = L.latLngBounds([-5120, -5120], [5120, 5120]);
+    var borderBounds = L.latLngBounds([-5000, -5000], [5000, 5000]);
+    var minZoom = -4;
+    return (
+      <RL.Map
+          className="map modal-container"
+          crs={mcCRS}
+          maxBounds={tileBounds}
+          center={Util.xz(this.state.view.x, this.state.view.z)}
+          zoom={this.state.view.zoom}
+          maxZoom={5}
+          minZoom={minZoom}
+          onmoveend={this.onmoveend.bind(this)}
+          onmousemove={this.onmousemove.bind(this)}
+          >
+
+        <RL.TileLayer
+          attribution={attribution}
+          url={tilesUrl + 'z{z}/{x},{y}.png'}
+          errorTileUrl={'img/no-tile.png'}
+          tileSize={256}
+          bounds={tileBounds}
+          minZoom={minZoom}
+          maxNativeZoom={0}
+          continuousWorld={true}
+          />
+
+        <CoordsDisplay cursor={this.state.cursorPos} />
+
+        <RL.LayersControl position='topright'>
+
+          <RL.LayersControl.Overlay name='world border' checked={true}>
+            <RL.Rectangle
+              bounds={borderBounds}
+              color='#ff8888'
+              fill={false}
+              />
+          </RL.LayersControl.Overlay>
+
+          <RL.LayersControl.Overlay name='world center'>
+            <RL.Marker position={[0, 0]} title='world center' />
+          </RL.LayersControl.Overlay>
+
+        </RL.LayersControl>
+      </RL.Map>
+    );
+  }
+}
+
+ReactDOM.render(
+  <McMap />,
+  document.getElementById('map-root')
+);
---
+++ js/util.js
@@ -0,0 +1,47 @@
+function getJSON(url, onData, onErr) {
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.onreadystatechange = function() {
+    if (this.readyState === 4) {
+      if (this.status >= 200 && this.status < 400) {
+        onData(JSON.parse(this.responseText));
+      } else {
+        onErr && onErr(this);
+      }
+    }
+  };
+  request.send();
+  request = null;
+}
+
+function xz(x, z) {
+  return [z, x];
+}
+
+function intCoords(point) {
+  var x = parseInt(point.lng);
+  var z = parseInt(point.lat);
+  if (point.lng < 0) x -= 1;
+  if (point.lat < 0) z -= 1;
+  return [z, x];
+}
+
+function viewToHash(leaf) {
+  var [z, x] = intCoords(leaf.getCenter());
+  return  '' + x + 'x/' + z + 'z/' + leaf.getZoom() + 'zoom';
+}
+
+function hashToView(hash) {
+  if (!hash) return {x: 0, z: 0, zoom: -4};
+  var [x, z, zoom] = hash.slice(1).split('/', 3)
+    .concat([0,0,0]);
+  return {x: parseFloat(x), z: parseFloat(z), zoom: parseFloat(zoom)};
+}
+
+module.exports = {
+  getJSON: getJSON,
+  xz: xz,
+  intCoords: intCoords,
+  viewToHash: viewToHash,
+  hashToView: hashToView,
+}
