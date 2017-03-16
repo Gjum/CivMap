aea3cca3c62163585c4aa7202ef319c84dc595ab
Gjum <code.gjum@gmail.com>
2017-03-16 04:28:06 +0100

    install plugins through options

--- js/app.js
+++ js/app.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {render} from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import Main from './Main';
+import {WaypointsPluginInfo} from './Waypoints';
+import {ClaimsPluginInfo} from './Claims';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
 exports.Main = Main;
+exports.ClaimsPlugin = ClaimsPluginInfo;
+exports.WaypointsPlugin = WaypointsPluginInfo;
 
 export function start(config) {
   render(
